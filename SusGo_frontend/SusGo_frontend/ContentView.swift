32ca9f15313d38460e6d032372c025e5a2b4bda0
Lily Pham <lnp35@cornell.edu>
2022-10-14 23:31:24 -0400

    Initialize frontend swift app

---
+++ SusGo_frontend/SusGo_frontend/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SusGo_frontend
+//
+//  Created by Lily Pham on 10/14/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SusGo_frontend/SusGo_frontend/SusGo_frontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  SusGo_frontendApp.swift
+//  SusGo_frontend
+//
+//  Created by Lily Pham on 10/14/22.
+//
+
+import SwiftUI
+
+@main
+struct SusGo_frontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
